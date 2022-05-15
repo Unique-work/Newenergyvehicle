1822ea72bba594ac296c304d7093e9c16ae1008d
Unique-work <19805062401@163.com>
2022-05-16 00:50:52 +0800

    v1.0.1 地图显示

--- src/main/java/com/siit/zsw/controller/IndexController.java
+++ src/main/java/com/siit/zsw/controller/IndexController.java
@@ -1,15 +1,35 @@
 package com.siit.zsw.controller;
 
+import com.siit.zsw.pojo.FaultInfo;
+import com.siit.zsw.service.CarLocationService;
+import com.siit.zsw.service.FaultInfoService;
+import com.siit.zsw.service.impl.CarLocationServiceImpl;
+import com.siit.zsw.service.impl.FaultSoltionServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
-
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    private FaultInfoService faultInfoService;
+    @Autowired
+    private CarLocationServiceImpl carLocationService;
+
+    @Autowired
+    private FaultSoltionServiceImpl faultSolutionService;
 
 
     @RequestMapping("index.do")
@@ -33,6 +53,16 @@ public class IndexController {
         return "Accset";
     }
 
+    @RequestMapping(value = "/Carposition.do", method = RequestMethod.GET)
+    public ModelAndView carposition(HttpServletRequest request,
+                                    HttpServletResponse response, ModelMap model) {
+        Map<String,Object> map = new HashMap<String,Object>();
+        List<FaultInfo> fau = faultInfoService.getfaultcount(map);
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("/WEB-INF/page/car/Carposition.jsp");
+        mv.addObject("fau",fau);
+        return mv;
+    }
 
 
 
---
+++ src/main/java/com/siit/zsw/service/ChartService.java
@@ -0,0 +1,36 @@
+package com.siit.zsw.service;
+
+import java.util.List;
+
+
+import com.siit.zsw.dao.DistributionMapper;
+import com.siit.zsw.dao.FaultMapper;
+import com.siit.zsw.pojo.Distribution;
+import com.siit.zsw.pojo.Fault;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+
+
+@Service
+public class ChartService {
+	
+	@Autowired
+	private FaultMapper faultMapper;
+	
+	@Autowired
+	private DistributionMapper distributionMapper;
+	
+
+	
+	public List<Fault> getFault(){
+		return faultMapper.getFault();
+	}
+	
+	public List<Distribution> getDistribution(){
+		return distributionMapper.getDistribution();
+	}
+	
+}
+
